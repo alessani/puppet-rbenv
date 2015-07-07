33d2db08489b92015b988dac3c9930b47c9c4aec
Matteo Alessani <alessani@gmail.com>
2015-07-07 18:37:25 +0200

    added gem

--- manifests/quasar.pp
+++ manifests/quasar.pp
@@ -5,6 +5,11 @@ class install_rbenv::quasar {
 
   rbenv::plugin { 'dcarley/rbenv-sudo': }
   
+  rbenv::gem {'passenger':
+    version => '5.0.13',
+    ruby_version => '2.2.2'
+  }
+  
   class {'passenger':
       passenger_version      => '5.0.13',
       passenger_provider     => 'gem',
@@ -12,10 +17,11 @@ class install_rbenv::quasar {
       gem_path               => '/home/ubuntu/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems',
       gem_binary_path        => '/home/ubuntu/.rbenv/versions/2.2.2/bin',
       passenger_root         => '/home/ubuntu/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems/passenger-5.0.13',
-      mod_passenger_location => '/home/ubuntu/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems/passenger-5.0.10/buildout/apache2/mod_passenger.so',
+      passenger_ruby         => '/home/ubuntu/.rbenv/versions/2.2.2/bin/ruby',
+      mod_passenger_location => '/home/ubuntu/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems/passenger-5.0.13/buildout/apache2/mod_passenger.so',
       include_build_tools    => true,
     }
     
-  Rbenv::Build['2.2.2'] -> Class[Passenger]
+  Rbenv::Build['2.2.2'] -> Rbenv::Gem['passenger'] -> Class[Passenger]
 
 }
