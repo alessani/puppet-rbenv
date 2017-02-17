d9e2a4eed30db44835bb1814170f86b564d2c811
Matteo Alessani <alessani@gmail.com>
2017-02-17 14:43:16 +0100

    passenger new

--- manifests/quasar.pp
+++ manifests/quasar.pp
@@ -3,25 +3,27 @@ class install_rbenv::quasar {
     global => true 
   }
 
+  rbenv::build { '2.2.2': }
+
   rbenv::plugin { 'dcarley/rbenv-sudo': }
   
   rbenv::gem {'passenger':
-    version => '5.0.13',
-    ruby_version => '2.2.2'
+    version => '5.1.2',
+    ruby_version => '2.3.3'
   }
   
   class {'passenger':
-      passenger_version      => '5.0.13',
+      passenger_version      => '5.1.2',
       passenger_provider     => 'gem',
       passenger_package      => 'passenger',
-      gem_path               => '/home/ubuntu/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems',
-      gem_binary_path        => '/home/ubuntu/.rbenv/versions/2.2.2/bin',
-      passenger_root         => '/home/ubuntu/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems/passenger-5.0.13',
-      passenger_ruby         => '/home/ubuntu/.rbenv/versions/2.2.2/bin/ruby',
-      mod_passenger_location => '/home/ubuntu/.rbenv/versions/2.2.2/lib/ruby/gems/2.2.0/gems/passenger-5.0.13/buildout/apache2/mod_passenger.so',
+      gem_path               => '/home/ubuntu/.rbenv/versions/2.3.3/lib/ruby/gems/2.3.0/gems',
+      gem_binary_path        => '/home/ubuntu/.rbenv/versions/2.3.3/bin',
+      passenger_root         => '/home/ubuntu/.rbenv/versions/2.3.3/lib/ruby/gems/2.3.0/gems/passenger-5.1.2',
+      passenger_ruby         => '/home/ubuntu/.rbenv/versions/2.3.3/bin/ruby',
+      mod_passenger_location => '/home/ubuntu/.rbenv/versions/2.3.3/lib/ruby/gems/2.3.0/gems/passenger-5.1.2/buildout/apache2/mod_passenger.so',
       include_build_tools    => false,
     }
     
-  Rbenv::Build['2.2.2'] -> Rbenv::Gem['passenger'] -> Class[Passenger]
+  Rbenv::Build['2.3.3'] -> Rbenv::Gem['passenger'] -> Class[Passenger]
 
 }
